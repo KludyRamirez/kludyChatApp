464c77016e9831d93f4deeb906d2dd4fea304526
Kludy Ramirez <ramirezkludy23@gmail.com>
2024-05-29 22:40:29 +0800

    [ADD] Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="w-[100%] h-[100%] flex justify-center items-center">
+      <Text>erwedsdssdadarwr</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
